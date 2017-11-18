441317ded2af717f08a44f4ea63084fda57ed920
tghelere <ghelere@outlook.com>
2017-11-18 20:33:18 -0200

    Configurações iniciais

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const watch = require('gulp-watch')
+
+gulp.task('sass', () => {
+	return gulp.src('sass/**/*.sass')
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(gulp.dest('css'))
+})
+
+gulp.task('watch', () => {
+	gulp.watch('sass/**/*.sass', ['sass'])
+})
+
+gulp.task('default', ['sass', 'watch'])
